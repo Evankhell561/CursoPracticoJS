0feafa29778cdfbef4d834c32fe6ce36f3214f39
Evankhell561 <49106866+Evankhell561@users.noreply.github.com>
2021-11-03 11:17:30 -0400

    cupones

---
+++ Cupones/descuentos.js
@@ -0,0 +1,27 @@
+const coupons = {
+    "cUpon1" : 10,
+    "cUpOn2" : 20,
+    "CuPoN3" : 30,
+    "cuPon4" : 100
+};
+
+function calcularPrecioConDescuento(precio, cupon) {
+    var descuento = coupons[cupon];
+    if(typeof descuento === "undefined") {
+        descuento = 0;
+    }
+    const porcentajePrecioDescuento = 100 - descuento;
+    const precioDescuento = precio * porcentajePrecioDescuento / 100;
+    return precioDescuento;
+}
+
+function calcularDescuento() {
+    const input1 = document.getElementById("inputPrecio");
+    const precio = parseInt(input1.value);
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+
+    const resultP = document.getElementById("resultP");
+    resultP.innerText = "el monto a pagar es: $" + calcularPrecioConDescuento(precio, couponValue);
+}
